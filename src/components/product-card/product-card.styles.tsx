78c852bf68d00c51efff9a8c4641f88759bdb8cb
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2022-10-21 17:02:16 +0200

    update style for mobile

--- src/components/product-card/product-card.styles.tsx
+++ src/components/product-card/product-card.styles.tsx
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 
-export const ProductCardContainer = styled.div`
+export const ProductCartContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   height: 350px;
   align-items: center;
   position: relative;
-
   img {
     width: 100%;
     height: 95%;
     object-fit: cover;
     margin-bottom: 5px;
   }
-
   button {
     width: 80%;
     opacity: 0.7;
@@ -22,15 +20,30 @@ export const ProductCardContainer = styled.div`
     top: 255px;
     display: none;
   }
-
   &:hover {
     img {
       opacity: 0.8;
     }
-
     button {
       opacity: 0.85;
       display: flex;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+    button {
+      display: block;
+      opacity: 0.9;
+      min-width: unset;
+      padding: 0 10px;
+    }
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
     }
   }
 `;
